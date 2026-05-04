6bbf163d76d7d2e188c0c7bcc9b00a7c86e740c3
MistarMano <ivanmenadamian2002@gmail.com>
2026-05-04 22:35:59 +0200

    Quitar cosas

--- src/Repositorio.java
+++ src/Repositorio.java
@@ -7,8 +7,7 @@ public interface Repositorio {
     public  int eliminar(String id);//involuicrta bases de datos y fichero
     public List<Obra> obtenerObras();//involuicrta bases de datos y fichero
     public List<Obra> obtenerMejorValoradas(int cantidad);//obtener as obras mas valoradas
-    public List<Manga> obtenerManga();//obtenemos todos los mangas
-    public List<Manhwa> obtenerManhwas();//obtenemos todos los manhwas
+
 
     //mostrar los mejores valorados mangas y manhwas 
 }
